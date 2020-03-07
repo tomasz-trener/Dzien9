fb65994acd068ac6a48d09c6cd6d17b71e490f2b
Tomasz <lest@ee.pw.edu.pl>
2020-03-07 11:19:25 +0100

    11:19

---
+++ P03ZadanieZawody/DaneDostepowe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03ZadanieZawody.Repo
+{
+    class DaneDostepowe
+    {
+        public static string ConnectionString =
+            System.Configuration.ConfigurationManager.
+                ConnectionStrings["polaczenieZBaza"].ConnectionString;
+        //   = "Data Source=.;Initial Catalog=A_zawodnicy;User ID=sa;Password=alx";
+    }
+}
---
+++ P03ZadanieZawody/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using P03ZadanieZawody.KlasyDomenowe;
+using P03ZadanieZawody.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P03ZadanieZawody
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ZawodnicyRepo zr = new ZawodnicyRepo();
+
+            Zawodnik[] zawodnicy =  zr.PodajWszystkichZawodnikow();
+
+            lbZawodnicy.DataSource = zawodnicy;
+            lbZawodnicy.DataValueField = "Id";
+            lbZawodnicy.DataTextField = "ImieNazwisko";
+            lbZawodnicy.DataBind();
+        }
+    }
+}
